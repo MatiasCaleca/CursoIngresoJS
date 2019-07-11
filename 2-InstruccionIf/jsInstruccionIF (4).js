e8c8f53dc8c396339a174ebfb5a6263039e75130
MatiasCaleca <52472575+MatiasCaleca@users.noreply.github.com>
2019-07-10 22:02:44 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
 
+    var edad;
+
+    edad = parseInt(document.getElementById("edad").value);
+
+    if(edad >=13  &&  edad <=17){
+
+        alert("Es adolecente");
+
+        
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,6 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 
+    var edad;
+
+    edad = parseInt(document.getElementById("edad").value);
+
+    if (edad >18){
+        alert("Es adulto");
+        
+    }
+    else if(edad > 13){
+        alert("Es adolecente");
+     
+    }    
+
+    else{
+        alert("Es niño")
+    
+    }
 
 
 
